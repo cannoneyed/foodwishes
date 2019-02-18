6b557af92b607c82c8549685742ca08f80323915
Andy Coenen <andrew.coenen@gmail.com>
2019-02-17 18:37:25 -0800

    Only show favorites when exist

--- src/components/DrawerContents/index.tsx
+++ src/components/DrawerContents/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { observer } from 'mobx-react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -8,6 +9,7 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import LocalPizzaIcon from '@material-ui/icons/LocalPizza';
 import SearchIcon from '@material-ui/icons/Search';
 import { withRouter, RouteComponentProps } from 'react-router';
+import { recipeStore } from '../../core/recipes';
 
 import styles from './styles.module.css';
 
@@ -24,6 +26,8 @@ class DrawerContents extends React.Component<Props, {}> {
   }
 
   render() {
+    const favorites = recipeStore.getFavorites();
+
     return (
       <div className={styles.list}>
         <List>
@@ -33,12 +37,14 @@ class DrawerContents extends React.Component<Props, {}> {
             </ListItemIcon>
             <ListItemText primary="Latest" />
           </ListItem>
-          <ListItem button onClick={this.navigateTo('/favorites')}>
-            <ListItemIcon>
-              <FavoriteIcon />
-            </ListItemIcon>
-            <ListItemText primary="Favorites" />
-          </ListItem>
+          {favorites.length ? (
+            <ListItem button onClick={this.navigateTo('/favorites')}>
+              <ListItemIcon>
+                <FavoriteIcon />
+              </ListItemIcon>
+              <ListItemText primary="Favorites" />
+            </ListItem>
+          ) : null}
           <ListItem button onClick={this.navigateTo('/categories')}>
             <ListItemIcon>
               <LocalPizzaIcon />
@@ -57,4 +63,4 @@ class DrawerContents extends React.Component<Props, {}> {
   }
 }
 
-export default withRouter(DrawerContents);
+export default withRouter(observer(DrawerContents));
