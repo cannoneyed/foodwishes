e044bd84087d1c951db7f72b3df69cfb93a1285c
Andy Coenen <andrew.coenen@gmail.com>
2019-02-18 16:46:56 -0800

    Updates

---
+++ precache-manifest.77d21dc46efd3a4441db8f4a81e11234.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b83c393278e4e5a8fda",
+    "url": "/static/js/runtime~main.0b83c393.js"
+  },
+  {
+    "revision": "aaa8be617ad57f24fc1b",
+    "url": "/static/js/main.aaa8be61.chunk.js"
+  },
+  {
+    "revision": "b9783e60b0b2195c0c56",
+    "url": "/static/js/2.b9783e60.chunk.js"
+  },
+  {
+    "revision": "aaa8be617ad57f24fc1b",
+    "url": "/static/css/main.6dc03b0d.chunk.css"
+  },
+  {
+    "revision": "31321041e70a23d2daeede9044667da5",
+    "url": "/index.html"
+  }
+];
