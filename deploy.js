6bea3012ea331b7791a121f1afa6697a9a484bc9
Andy Coenen <andrew.coenen@gmail.com>
2019-02-18 16:41:18 -0800

    Update single page app code

--- deploy.js
+++ deploy.js
@@ -1,8 +1,6 @@
 const ghPages = require('gh-pages');
 const fs = require('fs');
 
-// Add a 200.html file to help with client-side routing.
-fs.copyFileSync('public/index.html', 'build/200.html');
 // Copy the CNAME file for correct custom domain.
 fs.copyFileSync('CNAME', 'build/CNAME');
 
