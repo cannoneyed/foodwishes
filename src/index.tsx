a192d5e7f57b616a29de0b6ea8fd96f60d50e307
Andy Coenen <andrew.coenen@gmail.com>
2019-02-18 16:46:11 -0800

    Switch to hash based router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { BASENAME } from './core/config';
 
 import ByLabel from './pages/ByLabel';
